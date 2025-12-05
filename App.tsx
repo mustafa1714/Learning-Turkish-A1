46b31fd895a2cdb83074b62d08d9f21fd5e9ff0f
mustafa1714 <mustafa1714.dev@gmail.com>
2025-12-06 02:24:07 +0300

    logo

--- App.tsx
+++ App.tsx
@@ -816,12 +816,12 @@ export default function App() {
               </p>
             </div>
             <div className='w-10 h-10 bg-teal-50 rounded-full flex items-center justify-center border border-teal-100 overflow-hidden shadow-sm'>
-              {/* <span className="text-xl">🕌</span>  */}
-              <img
+              <span className='text-xl'>🕌</span>
+              {/* <img
                 src='/logo.webp'
                 alt='Al-Furqan Logo'
                 className='w-8 h-8 object-contain'
-              />
+              /> */}
             </div>
           </div>
         </div>
